242d5940dd79226d67e21a2dfea5cf2394b2f511
Towsif Ahmed Omi <towsif031@gmail.com>
2021-07-12 23:28:04 +0600

    basic card.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 function App() {
 	return (
-		<div>
-			<h1 className="text-3xl">Hello World!</h1>
+		<div className="max-w-sm rounded overflow-hidden shadow-lg">
+			<img
+				src="https://source.unsplash.com/random"
+				alt=""
+				className="w-full"
+			/>
+			<div className="px-6 py-4">
+				<div className="font-bold text-purple-500 text-xl mb-2">
+					Photo by John Doe
+				</div>
+				<ul>
+					<li>
+						<strong>Views: </strong>4000
+					</li>
+					<li>
+						<strong>Downloads: </strong>300
+					</li>
+					<li>
+						<strong>Likes: </strong>400
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
