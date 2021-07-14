80ceac7978e97b3502fbccffc1ac37cd6a24549e
Towsif Ahmed Omi <towsif031@gmail.com>
2021-07-14 23:04:32 +0600

    got api response using own api key.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+	const [images, setImages] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [term, setTerm] = useState('');
+
+	useEffect(() => {
+		fetch(
+			`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+		)
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err));
+	}, []);
+
 	return (
 		<div className="max-w-sm rounded overflow-hidden shadow-lg">
 			<img
