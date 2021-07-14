8286ac73fdb2dc3a33b44843eec4de8059caf742
Towsif Ahmed Omi <towsif031@gmail.com>
2021-07-15 01:22:10 +0600

    implemented search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
 	const [images, setImages] = useState([]);
@@ -16,10 +17,12 @@ function App() {
 				setIsLoading(false);
 			})
 			.catch((err) => console.log(err));
-	}, []);
+	}, [term]);
 
 	return (
 		<div className="container mx-auto">
+			<ImageSearch searchText={(text) => setTerm(text)} />
+
 			{isLoading ? (
 				<h1 className="text-6xl text-center mx-auto mt-32">
 					Loading...
